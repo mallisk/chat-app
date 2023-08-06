aaade8cbd6eb085fd8c237f93603259ef3a2bb5c
mallisk <malli.sk@yahoo.com>
2023-08-07 03:13:56 +0530

    created schemas

---
+++ backend/models/messageModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  {
+    timmestamps: true,
+  }
+);
+
+const Message = mongoose.model(messageModel);
+
+module.exports = Message;
