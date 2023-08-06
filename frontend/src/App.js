445f4ce7d767e0de29f24ca9f547554a819c2a68
mallisk <malli.sk@yahoo.com>
2023-08-07 01:28:08 +0530

    initil setup for backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="blue">HELLO</Button>
+    </div>
+  );
+}
+
+export default App;
